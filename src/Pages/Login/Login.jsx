3c971068c451216393e3c918263fc77ed24e5b77
careerpedia <somesh.11careerpedia>
2024-02-06 23:32:49 +0530

    Initial commit

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Login.css";
+import { Link } from 'react-router-dom';
+import { IoIosArrowForward } from "react-icons/io";
+import Section1 from "./Section1";
+import Faq from "./Faq";
+import Footer from "../../Components/Footer/Footer";
+
+const Login = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-content">
+          <div className="navbar-cont">
+            <img
+              src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png"
+              alt="netflix"
+            />
+
+            <div className="btns">
+                 <select  id="language">
+                 <option value="volvo">English</option>
+                 <option value="saab">Hindi</option>
+                 </select>
+              {/* <button>SIGN IN</button> */}
+              <Link to="./home"><button id="button">Movies</button></Link>
+              <Link to="./signup"><button id="button">SIGN IN</button></Link>
+              {/* <button id="button">SIGN UP</button> */}
+            </div>
+          </div>
+
+          <div className="hero-cont">
+            <h1>Enjoy big movies, hit series and more from ₹149.</h1>
+            <h3>Join today. Cancel anytime.</h3>
+            <h6>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h6>
+            <div className="mail">
+              <input type="email" placeholder="Email Address" />
+              <button>
+                Get Started{" "}
+                <IoIosArrowForward size={20} style={{ paddingTop: "5px" }} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Section1 />
+
+      <Faq />
+      <hr style={{ backgroundColor: "gray", height: "15px" }} />
+
+      
+
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
